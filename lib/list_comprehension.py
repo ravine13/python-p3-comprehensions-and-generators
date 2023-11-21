c291397780773255094256cf8ee2fb167c46cffc
ravine13 <kuriaderek@gmail.com>
2023-11-21 22:57:56 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+
+    for num in num_list:
+        if num % 2 == 0:
+            even_numbers.append(num)
+
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    modified_sentences = []
+
+    if not sentence_list:
+        return modified_sentences
+
+    for sentence in sentence_list:
+        modified_sentence = sentence + "!"  
+        modified_sentences.append(modified_sentence)  
+
+    return modified_sentences
